3776a8293bdde3d560bc6e033185120c3b42f329
Nicolas Pierrot <pierrotnicolas8@gmail.com>
2020-08-09 12:11:13 +0200

    first commit

---
+++ src/main/java/fr/bab/radiobackend/RadioBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.bab.radiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioBackendApplication.class, args);
+	}
+
+}
